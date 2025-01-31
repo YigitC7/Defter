65c31836e53d42021e3daded7cb6fb61295aee04
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-01-31 19:16:53 +0300

    Add files via upload

--- src/Defter.py
+++ src/Defter.py
@@ -1,31 +1,34 @@
 import customtkinter as ctk
-from os import getenv
-from os import mkdir
+from os import getenv, mkdir
 import os
 
 kullanici_dizini = os.path.expanduser("~")
 kullanici_adi = os.getlogin()
 
-hakkinda_text = """ |
- |Sürüm\t\t: 3.0
- |Yazan\t\t: Yiğit çıtak
- |Dil\t\t: Python
- |veri tabanı\t: .yigit
- |kütüphane\t: customtkinter
+hakkinda_text = """ |Sürüm\t\t\t: 4.0
+ |Karakter türü\t\t: UTF-8
+ |Yazan\t\t\t: Yiğit Çıtak
+ |Dil\t\t\t: Python
+ |veri tabanı\t\t: .yigit
+ |GUI kütüphanesi\t: Customtkinter
  |=======================================
  |Bu bir not defteri programıdır.
  |Sayfalar halinde not tutmanızı sağlar
- |ve sadece Linux (KDE Plasma uyumlu) işletim
- |sistemi için hazırlanmıştır. yazdığınız notlar
+ |ve sadece Linux işletim sistemi için
+ |hazırlanmıştır. yazdığınız notlar
  |Benim geliştirdiğim şifreli bir
  |veri tabaınında tutulur. Bu veri tabanı
  |kullanıcı klasöründe .defter içinde bulunur.
  |"""
+
 kisayol_text = """
  |CTRL + C : Kopyala
  |CTRL + V : Yapıştır
  |CTRL + X : Kes
- |CTRL + S : Kaydet"""
+ |CTRL + S : Kaydet
+ |CTRL + T : Temalar"""
+
+baslangic_ve_bittis_cizgisi = "===========[Defter"
 
 def Sifrele(sifrelenecek_girdi):
 	sifreleniyor1 = sifrelenecek_girdi.replace("q","y0y")
@@ -172,8 +175,9 @@ def text_size_oku():
 	return file_okundu
 	file.close()
 
-
 def defter_program():
+	global Yeniden_baslatma_islemi
+	Yeniden_baslatma_islemi = False
 
 	def kayit_fonk(event=None):
 		pad_icerik = yazi_paneli.get("1.0","end").strip()
@@ -191,9 +195,62 @@ def defter_program():
 				file.write(pad_icerik_y0)
 				file.close()
 
-	color_sayfalar = "#5314CD"
-	color_genel = "#5314CD"
-	color_window = "#d9e997"
+	class colors:
+		def __init__(self,
+			   sayfalar="#5314CD",
+			   genel="#5314CD",
+			   window="#d9e997",
+			   panel="#c7d492",
+			   yazi_panel = "#000099",
+			   title_text = "black",
+			   info_text = "white",
+			   kisa_text = "white",
+			   button_text = "white",
+			   sayfa_text = "white",
+			   yazi_paneli_text = "white",
+			   sayfa_numarasi_text = "black",
+			   kayit_bildirim_text = "blue",
+			   etkin_tema_text = "black"):
+			self.sayfalar = sayfalar
+			self.genel = genel
+			self.window = window
+			self.panel = panel
+			self.yazi_panel = yazi_panel
+			self.title_text = title_text
+			self.info_text = info_text
+			self.kisa_text = kisa_text
+			self.button_text = button_text
+			self.sayfa_text = sayfa_text
+			self.yazi_paneli_text = yazi_paneli_text
+			self.sayfa_numarasi_text = sayfa_numarasi_text
+			self.kayit_bildirim_text = kayit_bildirim_text
+			self.etkin_tema_text = etkin_tema_text
+
+	tema_index1 = colors()
+	tema_index2 = colors(
+		sayfalar = "#041538",
+		genel = "#363636",
+		window = "#1c1c1c",
+		panel = "#696969",
+		yazi_panel = "#080808",
+		title_text = "white",
+		sayfa_numarasi_text = "white",
+		kayit_bildirim_text = "#6495ed",
+		etkin_tema_text = "white",
+		yazi_paneli_text = "#dcdcdc"
+		)
+	tema_index3 = colors(
+		sayfalar = "#9C9C9C",
+		genel = "#dddddd",
+		panel = "#808080",
+		yazi_panel = "#2f2f2f",
+		window = "#F5F5F5",
+		kayit_bildirim_text = "#363636",
+		kisa_text = "black",
+		info_text = "black",
+		sayfa_text = "white",
+		button_text = "black",
+		)
 
 	try:
 		mkdir(f"{kullanici_dizini}/.defter")
@@ -202,18 +259,29 @@ def defter_program():
 	except:
 		print("Bilgi: Sistem kayıt dosyası algılandı")
 
+	def tema_index_yaz(girdi):
+		file = open(f"{kullanici_dizini}/.defter/tema_index.yigit","w")
+		file.write(girdi)
+		file.close()
+
+	def tema_index_oku():
+		file = open(f"{kullanici_dizini}/.defter/tema_index.yigit","r")
+		file_yaz = file.read()
+		return file_yaz
+		file.close()
+
 	def info_fonk(event=None):
 		hakkinda = ctk.CTk()
 		hakkinda.title("Defter | Hakkında")
 		hakkinda.minsize(390,340)
-		hakkinda.configure(fg_color=color_window)
+		hakkinda.configure(fg_color=main_tema.window)
 
 		info = ctk.CTkLabel(hakkinda,
 			text=hakkinda_text,
 			font=("italic",20),
-			text_color="white",
+			text_color=main_tema.info_text,
 			justify="left",
-			fg_color=color_genel)
+			fg_color=main_tema.genel)
 		info.pack(pady=20)
 
 		hakkinda.mainloop()
@@ -222,19 +290,128 @@ def defter_program():
 		hakkinda = ctk.CTk()
 		hakkinda.title("Defter | Kısayollar")
 		hakkinda.minsize(390,340)
-		hakkinda.configure(fg_color=color_window)
+		hakkinda.configure(fg_color=main_tema.window)
 
-		info = ctk.CTkLabel(hakkinda,
+		kisa = ctk.CTkLabel(hakkinda,
 			text=kisayol_text,
 			font=("italic",30),
-			text_color="white",
+			text_color=main_tema.kisa_text,
 			justify="left",
 			width=350,
 			height=300,
-			fg_color=color_genel)
-		info.pack(pady=20)
+			fg_color=main_tema.genel)
+		kisa.pack(pady=20)
 
 		hakkinda.mainloop()
+
+	def tema_fonk(event=None):
+		def geri(event = None):
+			window.bind("<Control-t>",tema_fonk)
+			window.title(baslik_turkce_optimasyon())
+			tema_button.configure(text="Temalar",command=tema_fonk,width=40)
+
+			not_paneli.place(relx=1.0, rely=0.5, anchor='e')
+			info_button.place(x=info_button_konum.x,y=info_button_konum.y)
+			yazi_paneli.place(x=yazi_paneli_konum.x,y=yazi_paneli_konum.y)
+			tema_yaziboyut_secenekleri.place(x=tema_yaziboyut_secenekleri_konum.x,y=tema_yaziboyut_secenekleri_konum.y)
+			kayit_button.place(x=kayit_button_konum.x,y=kayit_button_konum.y)
+			kisayol_button.place(x=kisayol_button_konum.x,y=kisayol_button_konum.y)
+			tema_button.place(x=tema_button_konum.x,y=tema_button_konum.y)
+			title.pack(pady=20)
+			sayfa_numarasi.place(x=sayfa_numarasi_konum.x,y=sayfa_numarasi_konum.y)
+
+			print("Bilgi: Tema ekranı 0")
+
+			tema_alani.pack_forget()
+			etkin_tema.pack_forget()
+
+		tema_button.configure(text="Geri",command=geri,width=80)
+		tema_button.place(x=10,y=10)
+
+		window.title(f"{baslik_turkce_optimasyon()} | Temalar")
+		window.bind("<Control-t>",geri)
+
+		not_paneli.place_forget()
+		info_button.place_forget()
+		yazi_paneli.place_forget()
+		tema_yaziboyut_secenekleri.place_forget()
+		kayit_button.place_forget()
+		kisayol_button.place_forget()
+		title.pack_forget()
+		sayfa_numarasi.place_forget()
+
+		print("Bilgi: Tema ekranı 1")
+
+		tema_alani = ctk.CTkScrollableFrame(window,
+				height=450,
+				width=650,
+				fg_color=main_tema.panel)
+		tema_alani.pack(pady=20)
+
+		def tema1_fonk():
+			tema_index_yaz("1")
+			print("Tema: 1")
+			global Yeniden_baslatma_islemi
+			Yeniden_baslatma_islemi = True
+			window.destroy()
+
+		tema1_button = ctk.CTkButton(tema_alani,
+				text="Klasik",
+				fg_color=main_tema.genel,
+				text_color=main_tema.button_text,
+				width=600,
+				height=50,
+				font=("italic",35),
+				command=tema1_fonk)
+		tema1_button.pack()
+
+		def tema2_fonk():
+			tema_index_yaz("2")
+			print("Tema: 2")
+			global Yeniden_baslatma_islemi
+			Yeniden_baslatma_islemi = True
+			window.destroy()
+
+		tema2_button = ctk.CTkButton(tema_alani,
+				text="Koyu",
+				fg_color=main_tema.genel,
+				text_color=main_tema.button_text,
+				width=600,
+				height=50,
+				font=("italic",35),
+				command=tema2_fonk)
+		tema2_button.pack()
+
+		def tema3_fonk():
+			tema_index_yaz("3")
+			print("Tema: 3")
+			global Yeniden_baslatma_islemi
+			Yeniden_baslatma_islemi = True
+			window.destroy()
+
+		tema3_button = ctk.CTkButton(tema_alani,
+				text="Açık",
+				fg_color=main_tema.genel,
+				text_color=main_tema.button_text,
+				width=600,
+				height=50,
+				font=("italic",35),
+				command=tema3_fonk)
+		tema3_button.pack()
+
+		etkin_tema = ctk.CTkLabel(window,
+				font=("italic",30),
+				text_color=main_tema.etkin_tema_text)
+		etkin_tema.pack(pady=25)
+
+		if tema_index_oku() == "1":
+			etkin_tema.configure(text="Etkin Tema: Klasik")
+		elif tema_index_oku() == "2":
+			etkin_tema.configure(text="Etkin Tema: Koyu")
+		elif tema_index_oku() == "3":
+			etkin_tema.configure(text="Etkin Tema: Açık")
+		else:
+			etkin_tema.configure(text="Tema bulunamadı :/")
 
 	def sayfa_oku():
 		file = open(f"{kullanici_dizini}/.defter/end.yigit","r")
@@ -244,17 +421,22 @@ def defter_program():
 		file.close()
 
 	try:
-		print(f"Geri döndü: {sayfa_oku()}")
+		print(f"sayfa geri döndü: {sayfa_oku()}")
 	except:
-		file8877 = open(f"{kullanici_dizini}/.defter/end.yigit","w")
-		file8877.write("1")
-		file8877.close()
+		file887755 = open(f"{kullanici_dizini}/.defter/end.yigit","w")
+		file887755.write("1")
+		file887755.close()
 
 	try:
-		print(f"yazı boyutu {text_size_oku()}")
+		print(f"Yazı boyutu geri döndü: {text_size_oku()}")
 	except:
 		text_size_yaz("20")
-		print("yazı boyutu ayarlandı: 20")
+		print("Yazı boyutu otomatik ayarlandı: 20")
+
+	try:
+		print(f"Tema index geri döndü: Tema {tema_index_oku()}")
+	except:
+		tema_index_yaz("1")
 
 	def baslik_turkce_optimasyon(kullanici_adi=kullanici_adi):
 		if not kullanici_adi.isalpha():
@@ -270,45 +452,58 @@ def defter_program():
 			sonuc = f"{kullanici_adi}'in defteri"
 			return sonuc.capitalize()
 
+	if tema_index_oku() == "1":
+		main_tema = tema_index1
+	elif tema_index_oku() == "2":
+		main_tema = tema_index2
+	elif tema_index_oku() == "3":
+		main_tema = tema_index3
+	else:
+		print("Hata: Tema index bug")
+
 	window = ctk.CTk()
 	window.title(baslik_turkce_optimasyon())
 	window.minsize(1200,750)
-	window.maxsize(1200,7500)
+	window.maxsize(1200,750)
 	window.geometry("1200x750x500x100")
-	window.configure(fg_color=color_window)
+	window.configure(fg_color=main_tema.window)
 	window.resizable(False, False)
 
 	window.bind("<Control-s>",kayit_fonk)
 	window.bind("<Control-h>",info_fonk)
+	window.bind("<Control-t>",tema_fonk)
 
 	class konum:
-		def __init__(self,x=0,y=0):
+		def __init__(self,x=1,y=1):
 			self.x = x
 			self.y = y
 
 	not_paneli_konum = konum(x=600,y=100)
-	info_button_konum = konum(x=1,y=1)
+	info_button_konum = konum()
 	yazi_paneli_konum = konum(x=1,y=150)
 	tema_yaziboyut_secenekleri_konum = konum(x=1,y=50)
-	kayit_button_konum = konum(x=175,y=10)
-	kisa_button_konum = konum(x=80,y=1)
+	kayit_button_konum = konum(x=235,y=10)
+	kisayol_button_konum = konum(x=80,y=1)
+	tema_button_konum = konum(x=160,y=1)
+	sayfa_numarasi_konum = konum(x=3,y=110)
 
 	title = ctk.CTkLabel(window,
 		text="Defter",
 		font=("italic",40),
-		text_color="black"
-		).pack(pady=20)
+		text_color=main_tema.title_text
+		)
+	title.pack(pady=20)
 
 	not_paneli = ctk.CTkScrollableFrame(window,
 		height=600,
 		width=300,
-		fg_color="#c7d492")
+		fg_color=main_tema.panel)
 	not_paneli.place(relx=1.0, rely=0.5, anchor='e')
 
 	info_button = ctk.CTkButton(window,
 		text="Hakkında",
-		fg_color=color_genel,
-		text_color="white",
+		fg_color=main_tema.genel,
+		text_color=main_tema.button_text,
 		width=40,
 		command=info_fonk)
 	info_button.place(
@@ -317,19 +512,29 @@ def defter_program():
 
 	kisayol_button = ctk.CTkButton(window,
 		text="Kısayollar",
-		fg_color=color_genel,
-		text_color="white",
+		fg_color=main_tema.genel,
+		text_color=main_tema.button_text,
 		width=40,
 		command=kisayol_fonk)
 	kisayol_button.place(
-		x=kisa_button_konum.x,
-		y=kisa_button_konum.y)
+		x=kisayol_button_konum.x,
+		y=kisayol_button_konum.y)
+
+	tema_button = ctk.CTkButton(window,
+		text="Temalar",
+		fg_color=main_tema.genel,
+		text_color=main_tema.button_text,
+		width=40,
+		command=tema_fonk)
+	tema_button.place(
+		x=tema_button_konum.x,
+		y=tema_button_konum.y)
 
 	yazi_paneli = ctk.CTkTextbox(window,
 		height=570,
 		width=860,
-		fg_color="#000099",
-		text_color="white",
+		fg_color=main_tema.yazi_panel,
+		text_color=main_tema.yazi_paneli_text,
 		wrap="word",
 		font=("italic",int(text_size_oku()))
 		)
@@ -340,7 +545,6 @@ def defter_program():
 	def yaziboyut_fonk(cuhice):
 		yazi_paneli.configure(font=("italic",int(cuhice)))
 		text_size_yaz(cuhice)
-
 
 	tema_values_l = [str(tema_values_i) for tema_values_i in range(10,41,2)]
 
@@ -356,18 +560,18 @@ def defter_program():
 	sayfa_numarasi = ctk.CTkLabel(window,
 		text="Sayfa 1",
 		font=("italic",20),
-		text_color="black")
-	sayfa_numarasi.place(x=3,y=110)
+		text_color=main_tema.sayfa_numarasi_text)
+	sayfa_numarasi.place(x=sayfa_numarasi_konum.x,y=sayfa_numarasi_konum.y)
 
 	kayit_bildirim = ctk.CTkLabel(window,
-							   text="Kayıt edildi!",
-							   font=("italic",21),
-							   text_color="blue")
+		text="Kayıt edildi!",
+		font=("italic",21),
+		text_color=main_tema.kayit_bildirim_text)
 
 	kayit_button = ctk.CTkButton(window,
 		text="Kaydet",
-		fg_color=color_genel,
-		text_color="white",
+		fg_color=main_tema.genel,
+		text_color=main_tema.button_text,
 		width=40,
 		font=("italic",21),
 		command=kayit_fonk)
@@ -375,8 +579,8 @@ def defter_program():
 		x=kayit_button_konum.x,
 		y=kayit_button_konum.y,)
 
-	for i in range(1, 101):
-		def create_page_function(page_num):
+	for sayfa_index in range(1, 101):
+		def sayfa_button_fonk(page_num):
 			def page_fonk():
 				file = open(f"{kullanici_dizini}/.defter/end.yigit", "w")
 				file.write(str(page_num))
@@ -388,19 +592,21 @@ def defter_program():
 					yazi_paneli.delete("1.0", "end")
 					yazi_paneli.insert("1.0", file2_islenmis2)
 					file2.close()
-				except FileNotFoundError:
+				except:
 					yazi_paneli.delete("1.0", "end")
+
 				sayfa_numarasi.configure(text=f"Sayfa {page_num}")
-				print(sayfa_oku())
+				print(f"sayfa: {sayfa_oku()}")
+
 			return page_fonk
 
-		button = ctk.CTkButton(not_paneli,
-						 text=f"Sayfa{i}",
-						 fg_color=color_sayfalar,
-						 text_color="white",
-						 width=500,height=50,
-						 command=create_page_function(i))
-		button.pack(pady=7)
+		sayfa_button = ctk.CTkButton(not_paneli,
+				text=f"Sayfa {sayfa_index}",
+				fg_color=main_tema.sayfalar,
+				text_color=main_tema.sayfa_text,
+				width=500,height=50,
+				command=sayfa_button_fonk(sayfa_index))
+		sayfa_button.pack(pady=7)
 
 	def sayfa_geri_dondur():
 		try:
@@ -418,4 +624,8 @@ def defter_program():
 	window.mainloop()
 
 if __name__ == "__main__":
+	print(baslangic_ve_bittis_cizgisi)
 	defter_program()
+	while Yeniden_baslatma_islemi:
+		defter_program()
+	print(baslangic_ve_bittis_cizgisi)
