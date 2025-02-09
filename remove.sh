7446ffb902638da05681208e6db6abebd936bd45
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-02-09 16:44:34 +0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -2,8 +2,8 @@ if [[ "${UID}" != 0 ]] ; then
 	echo -e "Lütfen bu scripti yönetici olarak çalıştırın: \n\tsudo bash ./notifcat.sh --install"
 	exit 1
 fi
-rm -rf install/Defter.py /usr/share/defter
-rm -rf install/defter.png /usr/share/pixmaps/
-rm -rf install/Defter.desktop /usr/share/applications/
+rm -rf /usr/share/defter
+rm -rf /usr/share/pixmaps/defter.png
+rm -rf /usr/share/applications/Defter.desktop
 clear
 echo -e "\e[1;34mDefter Programı Kaldırılmıştır\033[0m"
