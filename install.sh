20002bc28eaeeffe79271bd16289235c52c810f9
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-02-09 20:38:38 +0300

    Kurulum aracı güncellendi
    Bazı sorunlar düzeltilip eklemeler yapıldı

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
 if [[ "${UID}" != 0 ]] ; then
-	echo -e "Lütfen bu scripti yönetici olarak çalıştırın: \n\tsudo bash ./notifcat.sh --install"
+	echo -e "Lütfen bu scripti yönetici olarak çalıştırın: \n\tsudo bash ./install.sh"
 	exit 1
+fi
+if ! command -v pip3
+then
+    	clear
+    	echo -e "\033[1;31m
+Pip3 sisteminizde bulunamadı!\033[1;33m
+Gerekli araçların kurulumu için python3-pip paketi gereklidir :)
+
+\033[1;31mPip3 Kurulumu:
+\033[1;36m
+Debian/Ubuntu
+→ sudo apt install python3-pip
+\033[1;32m
+Arch Linux
+→ sudo pacman -S python3-pip
+\033[1;36m
+Fedora
+→ sudo yum install python3-pip\033[0m
+"
+exit 1
 fi
 pip3 install tkinter customtkinter
 mkdir /usr/share/defter
