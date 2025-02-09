bffde3bbcd91f56ec74fabdcb49da81caf52679b
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-02-09 16:37:32 +0300

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,5 @@
+rm -rf install/Defter.py /usr/share/defter
+rm -rf install/defter.png /usr/share/pixmaps/
+rm -rf install/Defter.desktop /usr/share/applications/
+clear
+echo -e "\e[1;34mDefter Programı Kaldırılmıştır\033[0m"
