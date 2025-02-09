c151c763bf6cf1e86ffea8a5a103cc8384712507
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-02-09 16:29:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ if [[ "${UID}" != 0 ]] ; then
 	echo -e "Lütfen bu scripti yönetici olarak çalıştırın: \n\tsudo bash ./notifcat.sh --install"
 	exit 1
 fi
+pip3 install tkinter customtkinter
 mkdir /usr/share/defter
 cp -r install/Defter.py /usr/share/defter
 cp -r install/defter.png /usr/share/pixmaps/
