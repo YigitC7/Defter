0976c0702e96acfcd8913a1d7f91f9da673cf476
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-04-13 13:34:50 +0300

    Update mk_deb.sh

--- mk_deb.sh
+++ mk_deb.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
 # Değişkenler
-PACKAGE_NAME="Defter"
+PACKAGE_NAME="defter"  # Paket isimleri genellikle küçük harfli olur
 PACKAGE_VERSION="6.0"
 PACKAGE_DIR="$PACKAGE_NAME-$PACKAGE_VERSION"
 
 # Paket dizinlerini oluştur
 mkdir -p "$PACKAGE_DIR/DEBIAN"
-mkdir -p "$PACKAGE_DIR/usr/local/bin"
-mkdir -p "$PACKAGE_DIR/usr/local/bin/defter"
-mkdir -p "$PACKAGE_DIR/usr/share/icons"
-mkdir -p "$PACKAGE_DIR/usr/local/share/applications"
+mkdir -p "$PACKAGE_DIR/usr/bin"
+mkdir -p "$PACKAGE_DIR/usr/share/icons/hicolor/128x128/apps"
+mkdir -p "$PACKAGE_DIR/usr/share/applications"
 
-# Kontrol dosyasını oluştur (boş)
+# Kontrol dosyasını oluştur
 cat << EOF > "$PACKAGE_DIR/DEBIAN/control"
 Package: $PACKAGE_NAME
 Version: $PACKAGE_VERSION
 Section: utils
 Priority: optional
 Architecture: amd64
-Depends: 
+Depends: libgtk-3-0, libwebkit2gtk-4.0-37
 Maintainer: YigitC7 <yigitcitak.1817@gmail.com>
 Description: Sayfalar halinde not tutmanın en güvenli yolu!
 EOF
 
-# Masaüstü dosyasını oluştur (boş)
-cat << EOF > "$PACKAGE_DIR/usr/local/share/applications/$PACKAGE_NAME.desktop"
+# Masaüstü dosyasını oluştur
+cat << EOF > "$PACKAGE_DIR/usr/share/applications/$PACKAGE_NAME.desktop"
 [Desktop Entry]
-Version=6.0
+Version=$PACKAGE_VERSION
 Type=Application
 Name=Defter
-Exec=/usr/local/bin/defter/Defter
-Icon=/usr/local/bin/defter/icon.png
+Exec=/usr/bin/Defter
+Icon=$PACKAGE_NAME
 Terminal=false
 Categories=Utility;Application;
 EOF
 
 # Dosyaları yerleştirme
-cp -r dist/Defter $PACKAGE_DIR/usr/local/bin/defter/Defter
-cp -f img/icon.png $PACKAGE_DIR/usr/local/bin/defter/icon.png
+cp -r dist/Defter "$PACKAGE_DIR/usr/bin/Defter"
+chmod +x "$PACKAGE_DIR/usr/bin/Defter"  # Çalıştırma izni ver
+cp -f img/icon.png "$PACKAGE_DIR/usr/share/icons/hicolor/128x128/apps/$PACKAGE_NAME.png"
 
-# Deb yapmak
-dpkg-deb --build $PACKAGE_DIR
+# Deb paketi oluştur
+dpkg-deb --build "$PACKAGE_DIR"
 
 echo "Dizin yapısı oluşturuldu: $PACKAGE_DIR"
 echo "Defter deb paketi oluşturuldu"
